17a0d22a620ada057cd4cab851e0b0c6b2101a16
Kim youngwoo <keiprogramo@gmail.com>
2025-01-16 19:12:45 +0900

    Texture 띄우기.

--- directX_3d/Engine/GeometryHelper.h
+++ directX_3d/Engine/GeometryHelper.h
@@ -5,5 +5,10 @@ class GeometryHelper
 {
 public:
 	static void CreateQuad(shared_ptr<Geometry<VertexColorData>> geometry, Color color);
+
+	static void CreateQuad(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateCube(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateSphere(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateGrid(shared_ptr<Geometry<VertexTextureData>> geometry);
 };
 
