124975df7a9c5bf70a546b8f2975a24db63b206f
Kim youngwoo <keiprogramo@gmail.com>
2025-01-22 23:56:32 +0900

    Ambient 추가

---
+++ directX_3d/Client/12. AmbientDemo.cpp
@@ -0,0 +1,78 @@
+#include "pch.h"
+#include "12. AmbientDemo.h"
+#include "GeometryHelper.h"
+#include "Camera.h"
+#include "CameraScript.h"
+#include "MeshRenderer.h"
+
+void AmbientDemo::Init()
+{	
+	RESOURCES->Init();
+	_shader = make_shared<Shader>(L"09. Lighting_Ambient.fx");
+	
+	// Camera
+	_camera = make_shared<GameObject>();
+	_camera->GetOrAddTransform();
+	_camera->AddComponent(make_shared<Camera>());
+	_camera->AddComponent(make_shared<CameraScript>());
+	_camera->GetTransform()->SetPosition(Vec3(0.0f, 0.0f, -10.0f));
+
+	// Object
+	_obj = make_shared<GameObject>();
+	_obj->GetOrAddTransform();
+	_obj->AddComponent(make_shared<MeshRenderer>());
+	{
+		_obj->GetMeshRenderer()->SetShader(_shader);
+	}
+	{
+		auto mesh = RESOURCES->Get<Mesh>(L"Sphere");
+		_obj->GetMeshRenderer()->SetMesh(mesh);
+	}
+	{
+		auto texture = RESOURCES->Load<Texture>(L"Orianna", L"..\\Resources\\Textures\\orianna.jpg");
+		_obj->GetMeshRenderer()->SetTexture(texture);
+	}
+
+	// Object2
+	_obj2 = make_shared<GameObject>();
+	_obj2->GetOrAddTransform()->SetPosition(Vec3{0.5, 0.f, 2.f});
+	_obj2->AddComponent(make_shared<MeshRenderer>());
+	{
+		_obj2->GetMeshRenderer()->SetShader(_shader);
+	}
+	{
+		auto mesh = RESOURCES->Get<Mesh>(L"Cube");
+		_obj2->GetMeshRenderer()->SetMesh(mesh);
+	}
+	{
+		auto texture = RESOURCES->Load<Texture>(L"Orianna", L"..\\Resources\\Textures\\orianna.jpg");
+		_obj2->GetMeshRenderer()->SetTexture(texture);
+	}
+
+	RENDER->Init(_shader);
+}
+
+void AmbientDemo::Update()
+{
+	_camera->Update();
+	RENDER->Update();
+
+	Vec4 lightAmbient{ 0.0f, 1.0f, 0.0f, 1.0f };
+	_shader->GetVector("LightAmbient")->SetFloatVector((float*)&lightAmbient);
+
+	{
+		Vec4 materialAmbient(0.1f);
+		_shader->GetVector("MaterialAmbient")->SetFloatVector((float*)&materialAmbient);
+		_obj->Update();
+	}
+
+	{
+		Vec4 materialAmbient(0.1f);
+		_shader->GetVector("MaterialAmbient")->SetFloatVector((float*)&materialAmbient);
+		_obj2->Update();
+	}
+}
+
+void AmbientDemo::Render()
+{
+}
